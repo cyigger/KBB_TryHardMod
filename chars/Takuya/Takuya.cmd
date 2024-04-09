09b44f2cf7121559d9ef4c9b4c6b1b7bd46fd7d4
JonTheCopycat <eljonnu0906@gmail.com>
2024-04-08 21:21:57 -0400

    Takuya Late Changes
    These are a bunch of changes that are also found on the changelog as of now.

--- chars/Takuya/Takuya.cmd
+++ chars/Takuya/Takuya.cmd
@@ -588,7 +588,7 @@ flag3=nostandguard
 [State -1, Soft Fast Fall]
 type = VelSet
 triggerall = statetype = A && movetype != H
-triggerall = stateno != [900,910] && stateno != 390 ;Moves that have been nerfed to not allow fastfall during them
+triggerall = stateno != [900,910] && stateno != 390 && stateno != [620,651] && stateno != [700,705] ;Moves that have been nerfed to not allow fastfall during them
 triggerall = command = "down" 
 triggerall = vel y > -1 && vel y < 6
 trigger1 = 1
